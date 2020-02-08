3bd268f65990fbaab80de310e1a392427d4d6eec
penguinpp <paldeepsingh21@gmail.com>
2020-02-08 18:20:56 +0800

    Display list of alcohol

--- rookery-bar-ui/src/App.js
+++ rookery-bar-ui/src/App.js
@@ -1,18 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import neo4j from 'neo4j-driver';
 
-function App() {
-  return (
-    <>
-      <p>Hello World</p>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-      </ul>
-    </>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    }
+  }
+
+  async getData() {
+    let resultData = []
+    let driver = await neo4j.driver(
+      '',
+      neo4j.auth.basic('', '')
+    )
+    let session = await driver.session()
+    session
+      .run('MATCH (n:Alcohol) RETURN n')
+      .then(result => {
+        result.records.forEach(record => {
+          resultData.push(
+            record.toObject().n.properties.name)
+        })
+      })
+      .then(() => {
+        this.setState({ data: resultData })
+      })
+      .catch(error => {
+        console.log(error)
+      })
+      .then(() => session.close())
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  render() {
+    return (
+      <>
+        <ul>
+          {this.state.data.map(item =>
+            <li>{item}</li>
+          )}
+        </ul>
+      </>
+    )
+  };
 }
 
 export default App;
